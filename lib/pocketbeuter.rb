12f28811c921795006eeb8f792bc35841c367370
Piotr Król <pietrushnic@gmail.com>
2013-12-10 00:36:41 +0100

    add config file to Pocketbeuter module

--- lib/pocketbeuter.rb
+++ lib/pocketbeuter.rb
@@ -1,24 +1,41 @@
 require "pocketbeuter/version"
 require "net/http"
 require "uri"
+require "launchy"
+require "get_access_token"
+require "fileutils"
+require "yaml"
 
 module Pocketbeuter
-
-
   ADD_URL = 'https://getpocket.com/v3/add'
   SEND_URL = 'https://getpocket.com/v3/send'
   GET_URL = 'https://getpocket.com/v3/get'
   OAUTH_URL = 'https://getpocket.com/v3/oauth/request'
+  AUTH_URL = 'https://getpocket.com/auth/authorize'
+  OAUTH_AUTH_URL = 'https://getpocket.com/v3/oauth/authorize'
 
-  class Runner
-    def self.start
-      uri = URI.parse("https://getpocket.com/v3/oauth/request")
-      http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Post.new(uri.request_uri)
-      request.set_form_data("consumer_key" => CONSUMER_KEY, "redirect_uri" => REDIRECT_URI)
-      http.use_ssl = true
-      res = http.request(request)
-      puts res.body
+  def config
+    @config ||= {}
+  end
+  def init
+    @config = YAML.load_file(File.expand_path('~/.pocketbeuter/config'))
+    config[:dir] = File.expand_path('~/.pocketbeuter')
+    config[:file] = config[:dir] + '/config'
+    puts config[:file]
+    GetAccessToken.get_access_token_and_username
+    save_config
+  end
+  def save_config
+    unless File.exists?(config[:dir])
+      FileUtils.mkdir_p(config[:dir])
+    end
+
+    if File.exists?(config[:file])
+      puts config.to_yaml
+      File.open(config[:file], 'w') { |f| f.write(config.to_yaml) }
+    else
+      File.open(config[:file], mode: 'w', perm: 0600).close
     end
   end
+  extend Pocketbeuter
 end
