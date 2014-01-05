2626e40b6b2257b3abc8bd2e6c91e635dcc3dc05
Piotr Król <pietrushnic@gmail.com>
2014-01-05 02:55:21 +0100

    use webmock and vcr

--- pocketbeuter.gemspec
+++ pocketbeuter.gemspec
@@ -24,4 +24,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "thor"
   spec.add_development_dependency "colorize"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "webmock"
+  spec.add_development_dependency "vcr"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,14 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.config do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
@@ -14,4 +22,5 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+  config.extend VCR::RSpec::Macros
 end
