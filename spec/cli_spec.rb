0e47b44a3809ac5ebcae4349bd0b2c745bdb17eb
Piotr Król <pietrushnic@gmail.com>
2014-01-11 00:56:42 +0100

    createapp and authorize tests

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,6 +1,7 @@
 require 'pocketbeuter'
 require 'helpers'
 require 'spec_helper'
+include WebMock::API
 
 describe Pocketbeuter::CLI do
   before :each do
@@ -16,24 +17,46 @@ describe Pocketbeuter::CLI do
     $stdout = $old_stdout
   end
 
-  describe '#create_app' do
+  describe '#createapp' do
     before do
-      File.open(fixtures_path + "/emptyrc", File::RDWR | File::TRUNC | File::CREAT, 0600) do |f|
+      File.open(fixtures_path + "/createapp", File::RDWR | File::TRUNC | File::CREAT, 0600) do |f|
         f.write ""
       end
-      @cli.config.path = fixtures_path + "/emptyrc"
+      @cli.config.path = fixtures_path + "/createapp"
     end
-    it 'print instructions and get consumer_key and redirect_uri' do
-      expect($stdout).to receive(:print).exactly(10).times
-      expect($stdin).to receive(:gets).and_return('n')
-      expect($stdout).to receive(:print).with('Enter consumer key:')
+    it 'print instructions and set account_name, consumer_key and redirect_uri' do
+      expect($stdout).to receive(:print)
+      expect($stdin).to receive(:gets).and_return('\n')
+      expect($stdout).to receive(:print).with("Enter account name [#{ENV['USER']}]: ")
+      expect($stdin).to receive(:gets).and_return('foo')
+      expect($stdout).to receive(:print).with('Enter consumer key: ')
       expect($stdin).to receive(:gets).and_return('foo1337')
-      expect($stdout).to receive(:print).with('Enter redirect uri:')
+      expect($stdout).to receive(:print).with('Enter redirect uri: ')
       expect($stdin).to receive(:gets).and_return('http://foo.bar')
-      $old_stdout.puts "config: #{@cli.config.default_account}"
       @cli.createapp
+      expect(@cli.config.account_name).to match(/foo/)
       expect(@cli.config.consumer_key).to match(/foo1337/)
       expect(@cli.config.redirect_uri).to match(/http:\/\/foo.bar/)
+    end
+  end
+  vcr_options = { :cassette_name => "authorize", :record => :new_episodes }
+  describe '#authorize', :vcr => vcr_options do
+    before do
+      File.open(fixtures_path + "/createapp", File::RDWR | File::TRUNC | File::CREAT, 0600) do |f|
+        f.write ""
+      end
+      @cli.config.path = fixtures_path + "/createapp"
+      @cli.config.account_name = 'foo'
+      @cli.config.consumer_key = 'foo1337'
+      @cli.config.redirect_uri = 'foo1337'
+    end
+    it 'request code, access_token and username' do
+      expect($stdout).to receive(:print)
+      expect($stdin).to receive(:gets).and_return('\n')
+      @cli.authorize
+      expect(@cli.config.code).to match(/foobar42/)
+      expect(@cli.config.access_token).to match(/barfoo42/)
+      expect(@cli.config.username).to match(/joedoe/)
     end
   end
 end
